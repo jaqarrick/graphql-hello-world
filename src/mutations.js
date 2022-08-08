9196620aea35a6a38e46806a59f521ccc93200dc
Jack Carrick <jcarrick@protonmail.com>
2022-08-08 14:08:42 -0700

    store books inside map

--- src/mutations.js
+++ src/mutations.js
@@ -1,17 +1,16 @@
 const { ApolloServer, gql } = require('apollo-server');
 
-const books = [
-    {
-        id: 0,
-        title: 'The Awakening',
-        author: 'Kate Chopin',
-    },
-    {
-        id: 1,
-        title: 'City of Glass',
-        author: 'Paul Auster',
-    },
-];
+
+const books = new Map()
+books.set("0", {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+})
+
+books.set("1", {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+})
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
@@ -21,7 +20,11 @@ const typeDefs = gql`
 
   # This "Book" type defines the queryable fields for every book in our data source.
   type Book {
-    id: ID!
+    title: String
+    author: String
+  }
+
+  input UpdateBookInput {
     title: String
     author: String
   }
@@ -37,28 +40,41 @@ const typeDefs = gql`
   type Mutation {
     addBook(title: String, author: String): Book
   }
-`;
 
+  type Mutation {
+    updateBook(id: ID!, input: UpdateBookInput ): Book
+  }
+`;
 
 // Resolvers define the technique for fetching the types defined in the
 // schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
     Query: {
-        books: () => books,
+        books: () => Array.from(books.values()),
         book: (_, { id }) => {
-            return books.find(book => book.id == id)
+            const book = books.get(id)
+            if (!book) throw "book not found"
+            return book
         }
     },
     Mutation: {
         addBook(_, payload) {
-            const storeBook = {
-                // just temporary way of creating a new id
-                id: books.length,
-                ...payload,
-            }
+            const bookId = books.size
+            books.set(bookId, payload)
+            return payload;
+        },
+        updateBook(_, payload) {
+            const { id, input } = payload
+            const bookToUpdate = books.get(id)
+            if (!bookToUpdate) throw "book not found"
 
-            books.push(storeBook)
-            return storeBook
+            books.set(id, {
+                ...bookToUpdate,
+                ...input
+            })
+
+            return books.get(id)
+
         }
     }
 };
